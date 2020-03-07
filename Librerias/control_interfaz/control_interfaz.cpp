c576cf7218324ec4b94ebae9edd0e9184a5992bb
tomaspoupin <54379361+tomaspoupin@users.noreply.github.com>
2020-03-07 14:23:20 -0300

    Add files via upload

---
+++ Librerias/control_interfaz/control_interfaz.cpp
@@ -0,0 +1,82 @@
+#include "di_types.h"
+#include <LedControl.h>
+
+#define MIN_INTERRUPT_TIME_INTERVAL 200
+#define BUTTON_NUMBER 4
+
+static DisplayInfo display_info;
+static Button buttons[BUTTON_NUMBER];
+static int empty_button_index = 0;
+static LedControl* p_display;
+
+static void update_display(DisplayInfo* di) {
+  p_display->setDigit(0, 3, di->display[3], false);
+  p_display->setDigit(0, 2, di->display[2], true);
+  p_display->setDigit(0, 1, di->display[1], false);
+  p_display->setDigit(0, 0, di->display[0], false);
+}
+
+
+// ------------------ Definicion Interfaz UI ------------------------
+void ui_init_display(const unsigned char data_in, const unsigned char clk, const unsigned char load_cs) {
+    INIT_DISPLAY_INFO(display_info);
+    static LedControl display = LedControl(data_in, clk, load_cs, 1);
+    display.shutdown(0, false);
+    display.setIntensity(0, 8);
+    display.clearDisplay(0);
+
+    p_display = &display;
+    p_display->setDigit(0, 3, 0x00, false);
+    p_display->setDigit(0, 2, 0x00, false);
+    p_display->setDigit(0, 1, 0x00, true);
+    p_display->setDigit(0, 0, 0x00, false);
+}
+
+void ui_init_buttons(const unsigned char* button_pins, const int button_number) {
+    int current_button;
+    for_each_button(current_button, button_number) {
+        pinMode(current_button, INPUT);
+    }
+}
+
+void ui_map_button(const unsigned char button_pin, const ButtonType button_type) {
+    Button new_button;
+    set_button_pin(&new_button, button_pin);
+    set_button_handler(&new_button, button_type);
+    buttons[empty_button_index] = new_button;
+    empty_button_index += 1;
+}
+
+Button* get_pressed_button();
+void ui_interrupt_handler() {
+    noInterrupts();
+    static int start_time = 0;
+    int interrupt_time = millis();
+    if ((interrupt_time - start_time) < MIN_INTERRUPT_TIME_INTERVAL) 
+        return;
+
+    // Comienza zona de interrupcion
+    Button* active_button = get_pressed_button();
+    active_button->handler(&display_info);
+    update_display(&display_info);
+    // termina zona de interrupcion
+
+    start_time = millis();
+    interrupts();
+}
+#define BUTTON_NOT_FOUND 1
+Button* get_pressed_button() {
+    int current_button = 0;
+    while(BUTTON_NOT_FOUND) {
+        if (is_button_high(&buttons[current_button])) {
+            // logging
+            Serial.println("Button pressed:");
+            Serial.println(buttons[current_button].pin);
+            Serial.println(" ");
+            return &buttons[current_button];
+        }
+        current_button += 1;
+        if (current_button == BUTTON_NUMBER)
+            current_button = 0;
+    }
+}
---
+++ Librerias/control_interfaz/di_types.h
@@ -0,0 +1,103 @@
+#ifndef UI_TYPES
+#define UI_TYPES
+
+#include "control_interfaz.h"
+#include <Arduino.h>
+
+// ------ ESTRUCTURAS ------------
+typedef struct {
+    int cursor; // entre 0 y 3
+    char display[4];
+} DisplayInfo;
+
+typedef struct {
+    unsigned char pin;
+    void (*handler)(DisplayInfo* di);
+} Button;
+
+// ------ BUTTON HANDLERS -----------
+
+#define CURSOR_CAN_CHANGE(di) (di->cursor < 3 && di->cursor > 0)
+static inline void di_right_button_handler(DisplayInfo* di) {
+    if (CURSOR_CAN_CHANGE(di))
+        di->cursor += 1;
+}
+static inline void di_left_button_handler(DisplayInfo* di) {
+    if (CURSOR_CAN_CHANGE(di))
+        di->cursor -= 1;
+}
+
+// README: EL di_top_button_handler puede que requiera modificacion, al igual
+// que el di_bottom_button_handler
+#define STEP_OPTION_1 1
+#define STEP_OPTION_2 2
+#define GET_MIN_STEP(cursor) ((cursor > 0)? STEP_OPTION_1 : STEP_OPTION_2)
+#define GET_CARRY(di, step) ((di->display[di->cursor] + step)/10)
+#define CANT_INCREMENT(di) \
+    (di->cursor == 3 && di->display[di->cursor] == 0x09)
+#define CAN_SUM_CARRY(di) (di->cursor < 3)
+#define CANT_DECREMENT(di) \
+    (di->display[di->cursor] == 0x00)
+
+static void di_top_button_handler(DisplayInfo* di) {
+    if(CANT_INCREMENT(di)) {}
+
+    else {
+        char step = GET_MIN_STEP(di->cursor);
+        char carry = GET_CARRY(di, step);
+        di->display[di->cursor] = (di->display[di->cursor] + step)%10;
+        if (CAN_SUM_CARRY(di))
+            di->display[di->cursor + 1] = \
+                di->display[di->cursor + 1] + carry;
+    }
+}
+static void di_bottom_button_handler(DisplayInfo* di) {
+    if(CANT_DECREMENT(di)) {}
+    
+    else {
+        char step = GET_MIN_STEP(di->cursor);
+        di->display[di->cursor] = (di->display[di->cursor] - step)%10;
+    }
+}
+
+// -------- DisplayInfo ----------------
+
+#define INIT_DISPLAY_INFO(display_info) \
+    display_info = {0, {0, 0, 0, 0}}
+
+// -------- Buttons --------------
+static inline void set_button_pin(Button* button, const unsigned char button_pin) {
+    button->pin = button_pin;
+}
+
+static void set_button_handler(Button* button, const ButtonType type) {
+    switch (type)
+    {
+    case UI_UP_BUTTON:
+        button->handler = di_top_button_handler;
+        break;
+    
+    case UI_RIGHT_BUTTON:
+        button->handler = di_right_button_handler;
+        break;
+
+    case UI_BOTTOM_BUTTON:
+        button->handler = di_bottom_button_handler;
+        break;
+
+    case UI_LEFT_BUTTON:
+        button->handler = di_left_button_handler;
+        break;
+    default:
+        break;
+    }
+}
+
+static inline bool is_button_high(Button* button) {
+    return (digitalRead(button->pin) == HIGH);
+}
+
+#define for_each_button(current_button, button_length) \
+    for(current_button = 0; current_button < button_length; current_button++)
+
+#endif
