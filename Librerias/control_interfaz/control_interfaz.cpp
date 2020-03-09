a4db314a7400b3b830d3a945a809d62607b98da9
Tomas Munoz <tomas.mpoupin@gmail.com>
2020-03-09 20:51:37 -0300

    Digito ahora parpadea

--- Librerias/control_interfaz/control_interfaz.cpp
+++ Librerias/control_interfaz/control_interfaz.cpp
@@ -1,5 +1,6 @@
 #include "di_types.h"
 #include <LedControl.h>
+#include <TimerOne.h>
 
 #define MIN_INTERRUPT_TIME_INTERVAL 200
 #define BUTTON_NUMBER 4
@@ -16,6 +17,44 @@ static void update_display(DisplayInfo* di) {
   p_display->setDigit(0, 0, di->display[0], false);
 }
 
+static void start_blink() {
+    Timer1.attachInterrupt(blink_callback);
+    Timer1.start();
+}
+static void stop_blink() {
+    Timer1.detachInterrupt();
+    Timer1.stop();
+}
+static void blink_callback() {
+    static int cursor = display_info.cursor;
+    static bool digit_state = 0;
+    static const int blink_amount = 5;
+    static int counter = 0;
+
+    if (display_info.cursor != cursor) {
+        cursor = display_info.cursor;
+        digit_state = 0;
+        cursor = display_info.cursor;
+    }
+    if (counter == blink_amount*2)
+    {
+        counter = 0;
+        stop_blink();
+    }
+
+    if (digit_state == 0) 
+        p_display->setChar(0,
+                            display_info.cursor,
+                            " ",
+                            display_info.cursor == 2);
+    else
+        p_display->setDigit(0,
+                            display_info.cursor, 
+                            display_info.display[display_info.cursor],
+                            display_info.cursor == 2);
+    digit_state = !digit_state;
+    counter++;
+}
 
 // ------------------ Definicion Interfaz UI ------------------------
 void ui_init_display(const unsigned char data_in, const unsigned char clk, const unsigned char load_cs) {
@@ -30,6 +69,9 @@ void ui_init_display(const unsigned char data_in, const unsigned char clk, const
     p_display->setDigit(0, 2, 0x00, false);
     p_display->setDigit(0, 1, 0x00, true);
     p_display->setDigit(0, 0, 0x00, false);
+
+    Timer1.initialize(200000);
+    Timer1.stop();
 }
 
 void ui_init_buttons(const unsigned char* button_pins, const int button_number) {
@@ -59,7 +101,7 @@ void ui_interrupt_handler() {
     Button* active_button = get_pressed_button();
     active_button->handler(&display_info);
     update_display(&display_info);
-    // termina zona de interrupcion
+    start_blink();
 
     start_time = millis();
     interrupts();
